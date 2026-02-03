f7a95f14bea44b07852d3af2c240bc8c38f91326
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2026-02-03 16:12:54 -0300

    add initial api requests structure

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,28 +2,57 @@
 
 import { Box, Button, Heading, Input, Stack, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { useState} from "react";
+import { useRouter } from "next/navigation";
+import { login } from "@/app/lib/api";
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const token = await login(username, password);
+            localStorage.setItem("token", token);
+            router.push("/");
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
-        <Stack gap={6}>
-            <Box textAlign="center">
-                <Heading size="2xl" mb={2}>Login</Heading>
-            </Box>
+        <form onSubmit={handleSubmit}>
+            <Stack gap={6}>
+                <Box textAlign="center">
+                    <Heading size="2xl" mb={2}>Login</Heading>
+                </Box>
 
-            <Stack gap={4}>
-                <Input placeholder="Email" type="email" size="lg" />
-                <Input placeholder="Password" type="password" size="lg" />
-                <Button colorPalette="blue" size="lg" width="full">Log In</Button>
+                <Stack gap={4}>
+                    <Input placeholder="Username" type="text" size="lg" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                    <Input placeholder="Password" type="password" size="lg" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    <Button colorPalette="blue" size="lg" width="full" type="submit" loading={isLoading}>Log In</Button>
+                </Stack>
+
+                {error && <Text color="red.500" textAlign="center">{error}</Text>}
+
+                <Text textAlign="center" fontSize="sm" color="fg.muted">
+                    Don't have an account?{" "}
+                    <Link href="/auth/register">
+                        <Button variant="plain" colorPalette="blue" size="sm">
+                            Sign up
+                        </Button>
+                    </Link>
+                </Text>
             </Stack>
-
-            <Text textAlign="center" fontSize="sm" color="fg.muted">
-                Don't have an account?{" "}
-                <Link href="/auth/register">
-                    <Button variant="plain" colorPalette="blue" size="sm">
-                        Sign up
-                    </Button>
-                </Link>
-            </Text>
-        </Stack>
+        </form>
     );
 }
---
+++ app/lib/api.ts
@@ -0,0 +1,37 @@
+const API_BASE_URL = process.env.EXPENSIFY_API_BASE_URL || 'http://localhost:8080';
+
+export async function register(username: string, password: string): Promise<void> {
+    const response = await fetch(`${API_BASE_URL}/auth/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Registration failed');
+    };
+
+    return;
+}
+
+export async function login(username: string, password: string): Promise<string> {
+    const response = await fetch(`${API_BASE_URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Login failed');
+    };
+
+    const data = await response.json();
+    
+    return data.token;
+}
